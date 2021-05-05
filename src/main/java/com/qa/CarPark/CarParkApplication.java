7c8412813dcba3acc09c1aa0b0f5e6dc422bead0
RyanGlennersterQA <rglennerster@qa.com>
2021-05-05 16:34:58 +0100

    RG: First Commit

---
+++ src/main/java/com/qa/CarPark/CarParkApplication.java
@@ -0,0 +1,13 @@
+package com.qa.CarPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarParkApplication.class, args);
+	}
+
+}
