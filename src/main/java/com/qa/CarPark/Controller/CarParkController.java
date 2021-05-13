057a5f46184ed4d8c019a30f8a5ed054be9377ac
RyanGlennersterQA <rglennerster@qa.com>
2021-05-13 12:06:35 +0100

    RG: Updated controller main class inline with testing

--- src/main/java/com/qa/CarPark/Controller/CarParkController.java
+++ src/main/java/com/qa/CarPark/Controller/CarParkController.java
@@ -2,20 +2,23 @@ package com.qa.CarPark.Controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.CarPark.Domain.CarPark;
 import com.qa.CarPark.service.CarParkService;
 
+@RestController
+@CrossOrigin
 public class CarParkController {
 	
 	private CarParkService service;
@@ -34,31 +37,17 @@ public class CarParkController {
 		return ResponseEntity.ok(this.service.getAll());
 	}
 	
-	@GetMapping("/getOne/{id}")
-	public ResponseEntity<CarPark> getCharacterById(@PathVariable Long id) {
-		return ResponseEntity.ok(this.service.getById(id));
-	}
-	
+
 	@DeleteMapping("/remove/{id}")
-	public CarPark removeCar(@PathVariable Long id) {
-		this.service.remove(id);
-		return this.service.getById(id);
+	public ResponseEntity<Boolean> removeCar(@PathVariable Long id) {
+		return ResponseEntity.ok(this.service.remove(id));
+		
 	}
 	
 	@PutMapping("/update/{id}")
 	public ResponseEntity<CarPark> updateCar(@PathVariable Long id, @RequestBody CarPark newCar) {
 		return new ResponseEntity<CarPark>(this.service.update(id, newCar), HttpStatus.ACCEPTED);
 	}
-	
-	@GetMapping("/findByMake")
-	public CarPark findByMake(@PathParam("make") String make) {
-		for (CarPark car : this.service.getAll()) {
-			System.out.println(car.getMake());
-		}
-		System.out.println(make);
-		return null;
-	}
-	
-	
+		
 
 }
