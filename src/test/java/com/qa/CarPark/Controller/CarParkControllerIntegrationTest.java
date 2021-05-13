a976c7c9a056b3b232f56eae772999a22e312565
RyanGlennersterQA <rglennerster@qa.com>
2021-05-13 12:07:43 +0100

    RG: Completed Unit and Integration Testing Classes

--- src/test/java/com/qa/CarPark/Controller/CarParkControllerIntegrationTest.java
+++ src/test/java/com/qa/CarPark/Controller/CarParkControllerIntegrationTest.java
@@ -1,8 +1,13 @@
 package com.qa.CarPark.Controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +15,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -19,6 +26,7 @@ import com.qa.CarPark.Domain.CarPark;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 public class CarParkControllerIntegrationTest {
 	
 	@Autowired
@@ -29,13 +37,13 @@ public class CarParkControllerIntegrationTest {
 	
 	@Test
 	void testCreate() throws Exception {
-		CarPark car = new CarPark ("Mercedes", "GLA", 05/06/2021, 05/07/2021);
+		CarPark car = new CarPark ("Mercedes", "GLA", "2021-05-11", "2021-05-12");
 		
 		String carAsJSON = this.mapper.writeValueAsString(car);
 		
 		RequestBuilder mockRequest = post("/create").contentType(MediaType.APPLICATION_JSON).content(carAsJSON);
 		
-		CarPark savedCar = new CarPark(1L, "Mercedes", "GLA", 05/06/2021, 05/07/2021);
+		CarPark savedCar = new CarPark(2L, "Mercedes", "GLA", "2021-05-11", "2021-05-12");
 		
 		String savedCarAsJSON = this.mapper.writeValueAsString(savedCar);
 		
@@ -45,8 +53,56 @@ public class CarParkControllerIntegrationTest {
 		
 		this.MockMvc.perform(mockRequest).andExpect(matchStatus).andExpect(matchBody);
 		
+	}
+	
+	@Test
+	void testGetAll() throws Exception {
+		CarPark savedCar = new CarPark(1L, "Mercedes", "GLA", "2021-05-11", "2021-05-12");
+		
+		List<CarPark> cars = List.of(savedCar);
+		RequestBuilder mockRequest = get("/getAll");
+		
+		
+		String savedCarAsJSON = this.mapper.writeValueAsString(cars);
+		
+		ResultMatcher matchBody = content().json(savedCarAsJSON);
+		ResultMatcher matchStatus = status().isOk();
+		this.MockMvc.perform(mockRequest).andExpect(matchBody).andExpect(matchStatus);
 		
 	}
+	
+	@Test
+	void testRemove() throws Exception {
+		RequestBuilder mockRequest = delete("/remove/1");
+		
+		ResultMatcher matchBody = content().string("true");
+		ResultMatcher matchStatus = status().isOk();
+		
+		this.MockMvc.perform(mockRequest).andExpect(matchBody).andExpect(matchStatus);
+		//status & content.
+	}
+	
+	@Test
+	void testUpdate() throws Exception {
+		
+		CarPark update = new CarPark("Mercedes", "GLC", "2021-05-13", "2021-05-14");
+		String updateAsJSON = this.mapper.writeValueAsString(update);
+		
+		RequestBuilder mockRequest = put("/update/1").contentType(MediaType.APPLICATION_JSON).content(updateAsJSON);
+		
+		CarPark updatedCar = new CarPark(1L, "Mercedes", "GLC", "2021-05-13", "2021-05-14");
+		String updatedCarAsJSON = this.mapper.writeValueAsString(updatedCar);
+		
+		ResultMatcher matchBody = content().json(updatedCarAsJSON);
+		ResultMatcher matchStatus = status().isAccepted();
+		
+		this.MockMvc.perform(mockRequest).andExpect(matchBody).andExpect(matchStatus);
+		
+		
+		
+	}
+	
+	
 	
 
 }
--- src/test/java/com/qa/CarPark/Service/CarParkServiceUnitTest.java
+++ src/test/java/com/qa/CarPark/Service/CarParkServiceUnitTest.java
@@ -1,5 +1,73 @@
 package com.qa.CarPark.Service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.CarPark.service.CarParkService;
+import com.qa.CarPark.Domain.CarPark;
+import com.qa.CarPark.repo.CarParkRepo;
+
+@SpringBootTest
 public class CarParkServiceUnitTest {
 
+	@Autowired
+	private CarParkService service;
+	
+	@MockBean
+	private CarParkRepo repo;
+	
+	@Test
+	void testCreate() {
+		CarPark car = new CarPark("Mercedes", "GLA", "2021-05-11", "2021-05-12");
+		CarPark savedCar = new CarPark(1L, "Mercedes", "GLA", "2021-05-11", "2021-05-12");
+		
+		Mockito.when(this.repo.save(car)).thenReturn(savedCar);
+		
+		assertThat(this.service.create(car)).isEqualTo(savedCar);
+		
+		Mockito.verify(this.repo, Mockito.times(1)).save(car);
+		
+	}
+	
+	@Test
+	void testGetAll() {
+		List<CarPark> list = this.service.getAll();
+		list.add(new CarPark(1L, "Mercedes", "GLA", "2021-05-11", "2021-05-12"));
+		
+		assertThat(list).size().isGreaterThan(0);
+		
+		Mockito.when(this.repo.findAll()).thenReturn(list);
+		
+		assertThat(this.service.getAll().isEmpty()).isFalse();
+		
+		Mockito.verify(this.repo, Mockito.atLeastOnce()).findAll();
+}
+	
+	@Test
+	void testUpdate() {
+		CarPark car = new CarPark(1L, "Mercedes", "GLA", "2021-05-11", "2021-05-12");
+		
+		Mockito.when(this.repo.findById(car.getId())).thenReturn(Optional.of(car));
+		Mockito.when(this.repo.save(car)).thenReturn(car);
+		
+		assertThat(this.service.update(car.getId(), car)).isEqualTo(car);
+		
+		Mockito.verify(this.repo, Mockito.atLeastOnce()).findById(car.getId());
+		Mockito.verify(this.repo, Mockito.atLeastOnce()).save(car);
+	}
+	
+	@Test
+	void testDelete() {
+		this.repo.deleteById(1L);
+		assertThat(this.repo.existsById(1L)).isFalse();
+	}
+	
 }
